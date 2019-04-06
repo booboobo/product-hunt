77601017eb1a3407541da4c3bc1bd9e42211b309
wanggang <chengnanhuakaile@gmail.com>
2019-04-06 16:30:36 +0800

    设置了static

--- hunt/settings.py
+++ hunt/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'product/static')
+]
+# STATIC_URL = '/static/'
